bd69c25848e2941e4ac7d4a735e443edeb3cccf2
Maxim Zakharov <dp.maxime@gmail.com>
2018-10-25 12:00:47 +1100

    WIP: tester modified for websocket proxy

--- tester/tester.go
+++ tester/tester.go
@@ -14,11 +14,22 @@ import (
 )
 
 func PingNodesN(participants []*peers.Peer, p peers.PubKeyPeers, n uint64, serviceAddress string) {
+	proxies := make(map[int]*lachesis.WebsocketLachesisProxy)
+	for _, participant := range participants {
+		node := p[participant.PubKeyHex]
+		addr := fmt.Sprintf("%s:%d", strings.Split(node.NetAddr, ":")[0], 9000)
+		proxy, err := lachesis.NewWebsocketLachesisProxy(addr, nil, 10*time.Second, nil)
+		if err != nil {
+			fmt.Printf("error:\t\t\t%s\n", err.Error())
+			fmt.Printf("Failed to create WebsocketLachesisProxy:\t\t\t%s (id=%d)\n", participant.NetAddr, node.ID)
+		}
+		proxies[node.ID] = proxy
+	}
 	for iteration := uint64(0); iteration < n; iteration++ {
 		participant := participants[rand.Intn(len(participants))]
 		node := p[participant.PubKeyHex]
 
-		_, err := transact(*participant, node.ID, serviceAddress)
+		_, err := transact(proxies[node.ID])
 
 		if err != nil {
 			fmt.Printf("error:\t\t\t%s\n", err.Error())
@@ -33,12 +44,7 @@ func PingNodesN(participants []*peers.Peer, p peers.PubKeyPeers, n uint64, servi
 	fmt.Println("Pinging stopped")
 }
 
-func transact(target peers.Peer, nodeId int, proxyAddress string) (string, error) {
-	addr := fmt.Sprintf("%s:%d", strings.Split(target.NetAddr, ":")[0], 9000)
-	proxy, err := lachesis.NewWebsocketLachesisProxy(addr, nil, 10*time.Second, nil)
-	if err != nil {
-		return "", err
-	}
+func transact(proxy *lachesis.WebsocketLachesisProxy) (string, error) {
 
 	// Ethereum txns are ~108 bytes. Bitcoin txns are ~250 bytes. We'll assume
 	// our txns are ~120 bytes in size
